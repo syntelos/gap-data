99ae72d2469d69b0632bb9742cc6b9e53d5b7bda
jdp <jdp@wtkx.org>
2009-09-14 16:21:53 +0000

    1.0.8

--- src/gap/Version.java
+++ src/gap/Version.java
@@ -39,7 +39,7 @@ public final class Version {
     /**
      * Build sequence number.
      */
-    public final static int Build = 7;
+    public final static int Build = 8;
     /**
      * HTTP Product Token plus comment build date and time.  
      * As in <code>" Major '.' Minor '.' Build "</code>.
