926ba775939caf7be6f86f09df5330ac37d35e14
jdp <jdp@wtkx.org>
2009-10-08 05:11:35 +0000

    upd 1.0.16

--- lib/gap-jbxml/src/jbxml/Version.java
+++ lib/gap-jbxml/src/jbxml/Version.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 John Pritchard and the Jela Project Group
+ * Copyright (c) 2009 John Pritchard, JBXML Project Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,15 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package jela;
+package jbxml;
 
 public final class Version
     extends Object
 {
-    public final static String Name = "jela";
-    public final static int Major   =  1;
+    public final static String Name = "gap-jbxml";
+    public final static int Major   =  0;
     public final static int Minor   =  0;
-    public final static int Build   =  1;
+    public final static int Build   =  5;
 
 
     public final static String Number = String.valueOf(Major)+'.'+String.valueOf(Minor);
